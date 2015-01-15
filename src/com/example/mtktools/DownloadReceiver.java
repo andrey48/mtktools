5d15dda8adcb92d1671d6ec33cd2a92107092a70
andrey48 <andrey48@gmail.com>
2015-01-15 17:29:56 +0300

    mtktools

--- src/com/example/mtktools/DownloadReceiver.java
+++ src/com/example/mtktools/DownloadReceiver.java
@@ -4,10 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class DownloadReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, intent.getAction() + " * " + intent.getDataString(), Toast.LENGTH_LONG).show();
         Log.d(MainActivity.LOG_TAG, intent.getAction() + " * " + intent.getDataString());
     }
 }
--- src/com/example/mtktools/MainActivity.java
+++ src/com/example/mtktools/MainActivity.java
@@ -16,9 +16,16 @@ import android.widget.Switch;
 import android.widget.TabHost;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MainActivity extends Activity {
     public static final String NOTIFICATION_LIGHT = "notification_light_pulse";
     public static final String LOG_TAG = "MTKTOOLS";
+    public static final String MY_DOWNLOADS = "content://downloads/my_downloads";
+    public static String[] EPO_FILES = {"/EPO.DAT", "/EPO.MD5"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,20 +63,35 @@ public class MainActivity extends Activity {
 
     }
     public void onButtonClick(View view) {
-        ContentValues values = new ContentValues();
-        String uri = ((EditText) findViewById(R.id.etEPO)).getText().toString().concat("/EPO.DAT");
-        values.put("uri", uri);
-        values.put("notificationpackage", getPackageName());
-        //values.put("notificationclass", DownloadReceiver.class.getCanonicalName());
-        Log.d(LOG_TAG, DownloadReceiver.class.getCanonicalName());
-        values.put("is_public_api", true);
-        values.put("allowed_network_types", ~0);
-        values.put("visibility", 1);
-        values.put("hint", "file://" + Environment.getExternalStorageDirectory().toString() + "/Download/EPO.DAT");
-        values.put("destination", 4);
+        for (String epo_file : EPO_FILES) {
+            String uri = ((EditText) findViewById(R.id.etEPO)).getText().toString().concat(epo_file);
+            ContentValues values = new ContentValues();
+
+            Log.d(LOG_TAG, DownloadReceiver.class.getCanonicalName());
+            Log.d(LOG_TAG, Uri.fromFile(getExternalCacheDir()).toString().concat(epo_file));
+
+            try {
+                File file = new File(getExternalCacheDir().toString().concat(epo_file));
+                if (!file.delete()) Log.e(LOG_TAG, "Can not delete " + file.getName());
+                values.put("uri", uri);
+                values.put("notificationpackage", getPackageName());
+                //values.put("notificationclass", DownloadReceiver.class.getCanonicalName());
+                values.put("is_public_api", true);
+                values.put("allowed_network_types", ~0);
+                values.put("visibility", 1);
+                values.put("hint", Uri.fromFile(getExternalCacheDir()).toString().concat(epo_file));
+                values.put("destination", 4);
+                Uri content = getContentResolver().insert(Uri.parse(MY_DOWNLOADS), values);
+                Log.d(LOG_TAG, content.toString());
+            } catch (RuntimeException e) {
+                Log.e(LOG_TAG, e.getMessage());
+            }
+        }
+
+    }
+    public void onButtonClick1(View view) {
         try {
-            Uri content = getContentResolver().insert(Uri.parse("content://downloads/my_downloads"), values);
-            Log.d(LOG_TAG, content.toString());
+            getContentResolver().delete(Uri.parse(MY_DOWNLOADS),"is_public_api = true",null);
         } catch (RuntimeException e) {
             Log.e(LOG_TAG, e.getMessage());
         }
