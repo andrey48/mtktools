e869ae6c84a167a319bec978a63fc68294b37187
andrey48 <andrey48@gmail.com>
2015-01-20 17:48:38 +0300

    Working EPO download

---
+++ src/com/example/mtktools/Consts.java
@@ -0,0 +1,11 @@
+package com.example.mtktools;
+
+public final class Consts {
+    public static final String NOTIFICATION_LIGHT = "notification_light_pulse";
+    public static final String LOG_TAG = "MTKTOOLS";
+    public static final String[] EPO_FILES = {"/EPO.DAT", "/EPO.MD5"};
+
+    private Consts(){
+        throw new AssertionError();
+    }
+}
--- src/com/example/mtktools/MainActivity.java
+++ src/com/example/mtktools/MainActivity.java
@@ -1,100 +1,178 @@
 package com.example.mtktools;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.ContentValues;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
+import android.content.DialogInterface;
+import android.os.*;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TabHost;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.*;
+import java.lang.Process;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
-    public static final String NOTIFICATION_LIGHT = "notification_light_pulse";
-    public static final String LOG_TAG = "MTKTOOLS";
-    public static final String MY_DOWNLOADS = "content://downloads/my_downloads";
-    public static String[] EPO_FILES = {"/EPO.DAT", "/EPO.MD5"};
+    ProgressDialog mProgressDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        TabHost tabs = (TabHost)findViewById(R.id.tabHost);
-        tabs.setup();
-        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
-        spec.setContent(R.id.linearLayout);
-        spec.setIndicator(getString(R.string.tab1_title));
-        tabs.addTab(spec);
-        spec = tabs.newTabSpec("tag2");
-        spec.setContent(R.id.linearLayout2);
-        spec.setIndicator(getString(R.string.tab2_title));
-        tabs.addTab(spec);
-        tabs.setCurrentTab(0);
-
+        // init switch
         Switch sw = (Switch) findViewById(R.id.swNotificationLight);
         if (sw != null) {
             try {
-                sw.setChecked(Settings.System.getInt(getContentResolver(), NOTIFICATION_LIGHT) != 0);
+                sw.setChecked(Settings.System.getInt(getContentResolver(), Consts.NOTIFICATION_LIGHT) != 0);
             } catch (Settings.SettingNotFoundException e) {
                 sw.setEnabled(false);
             }
         }
+        // init progress dialog
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        //mProgressDialog.setTitle(getString(R.string.download_msg));
+        mProgressDialog.setMessage(getString(R.string.download_msg));
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setCancelable(true);
     }
+
     public void onSwitchClick(View view) {
         if (((Switch) view).isChecked()) {
-            Settings.System.putInt(getContentResolver(), NOTIFICATION_LIGHT, 1);
+            Settings.System.putInt(getContentResolver(), Consts.NOTIFICATION_LIGHT, 1);
             Toast.makeText(view.getContext(), getString(R.string.notification_light_pulse) + " " + getString(R.string.on), Toast.LENGTH_LONG).show();
         } else {
-            Settings.System.putInt(getContentResolver(), NOTIFICATION_LIGHT, 0);
+            Settings.System.putInt(getContentResolver(), Consts.NOTIFICATION_LIGHT, 0);
             Toast.makeText(view.getContext(), getString(R.string.notification_light_pulse) + " " + getString(R.string.off), Toast.LENGTH_LONG).show();
         }
-
     }
+
     public void onButtonClick(View view) {
-        for (String epo_file : EPO_FILES) {
-            String uri = ((EditText) findViewById(R.id.etEPO)).getText().toString().concat(epo_file);
-            ContentValues values = new ContentValues();
+        final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
 
-            Log.d(LOG_TAG, DownloadReceiver.class.getCanonicalName());
-            Log.d(LOG_TAG, Uri.fromFile(getExternalCacheDir()).toString().concat(epo_file));
+        downloadTask.execute(((EditText) findViewById(R.id.etEPO)).getText().toString());
+        mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                downloadTask.cancel(true);
+            }
+        });
+    }
 
+    public static void sudo(String...strings) {
+        try{
+            Process su = Runtime.getRuntime().exec("su");
+            DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+            for (String s : strings) {
+                outputStream.writeBytes(s + "\n");
+                outputStream.flush();
+            }
+            outputStream.writeBytes("exit\n");
+            outputStream.flush();
             try {
-                File file = new File(getExternalCacheDir().toString().concat(epo_file));
-                if (!file.delete()) Log.e(LOG_TAG, "Can not delete " + file.getName());
-                values.put("uri", uri);
-                values.put("notificationpackage", getPackageName());
-                //values.put("notificationclass", DownloadReceiver.class.getCanonicalName());
-                values.put("is_public_api", true);
-                values.put("allowed_network_types", ~0);
-                values.put("visibility", 1);
-                values.put("hint", Uri.fromFile(getExternalCacheDir()).toString().concat(epo_file));
-                values.put("destination", 4);
-                Uri content = getContentResolver().insert(Uri.parse(MY_DOWNLOADS), values);
-                Log.d(LOG_TAG, content.toString());
-            } catch (RuntimeException e) {
-                Log.e(LOG_TAG, e.getMessage());
+                su.waitFor();
+            } catch (InterruptedException e) {
+                Log.e(Consts.LOG_TAG, e.getMessage());
+                //Toast.makeText(context , e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            outputStream.close();
+        } catch(IOException e){
+            Log.e(Consts.LOG_TAG, e.getMessage());
+            //Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+        private static final int BUFFER_SIZE = 4096;
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+
+        public DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            for (String epo_file : Consts.EPO_FILES) {
+                InputStream input = null;
+                OutputStream output = null;
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(sUrl[0] + epo_file);
+                    Log.d(Consts.LOG_TAG, url.toString());
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                        return "Server returned HTTP " + connection.getResponseCode() + " " + connection.getResponseMessage();
+                    }
+                    Log.d(Consts.LOG_TAG, connection.getHeaderField("Last-Modified"));
+                    int fileLength = connection.getContentLength();
+                    input = connection.getInputStream();
+                    output = new FileOutputStream(getFilesDir().getPath() + epo_file);
+                    byte data[] = new byte[BUFFER_SIZE];
+                    long total = 0;
+                    int count;
+                    while ((count = input.read(data)) != -1) {
+                        if (isCancelled()) {
+                            input.close();
+                            return null;
+                        }
+                        total += count;
+                        if (fileLength > 0) publishProgress((int) (total * 100 / fileLength));
+                        output.write(data, 0, count);
+                    }
+                } catch (Exception e) {
+                    return e.toString();
+                } finally {
+                    try {
+                        if (output != null) output.close();
+                        if (input != null) input.close();
+                    } catch (IOException e) {
+                        Log.e(Consts.LOG_TAG, e.toString());
+                    }
+                    if (connection != null) connection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // take CPU lock to prevent CPU from going off if the user
+            // presses the power button during download
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            mWakeLock.acquire();
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mWakeLock.release();
+            mProgressDialog.dismiss();
+            if (result != null) Toast.makeText(context, "Download error: " + result, Toast.LENGTH_LONG).show();
+            else Toast.makeText(context, "File downloaded", Toast.LENGTH_SHORT).show();
+            try {
+                Toast.makeText(context, "EPO updated", Toast.LENGTH_SHORT).show();
+                sudo("cp -v " + getFilesDir().getPath() + Consts.EPO_FILES[0] + " /data/misc");
+            } catch (Exception e) {
+                Log.e(Consts.LOG_TAG, e.getMessage());
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
-
     }
-    public void onButtonClick1(View view) {
-        try {
-            getContentResolver().delete(Uri.parse(MY_DOWNLOADS),"is_public_api = true",null);
-        } catch (RuntimeException e) {
-            Log.e(LOG_TAG, e.getMessage());
-        }
-    }
-
 }
